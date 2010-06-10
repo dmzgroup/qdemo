7e041107629fe97347185b37d7d4cdbcb5627fce
Randall E. Barker <simstorm@mac.com>
2010-06-09 17:00:49 -0700

    first commit

---
+++ spectator.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+. ../scripts/envsetup.sh
+
+$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/render.xml config/runtime.xml config/resource.xml config/common.xml config/input.xml config/spectator.xml $*
