bf5ce7c7597030180e71c8ab9aa1ac455973b79a
Randall E. Barker <simstorm@mac.com>
2010-06-15 16:57:40 -0700

    Added simple location autoring tool.

---
+++ scripts/author.js
@@ -0,0 +1,86 @@
+var dmz =
+       { object: require("dmz/components/object")
+       , objectType: require("dmz/runtime/objectType")
+       , overlay: require("dmz/components/overlay")
+       , portal: require("dmz/components/portal")
+       , input: require("dmz/components/input")
+       , time: require("dmz/runtime/time")
+       , vector: require("dmz/types/vector")
+       , matrix: require("dmz/types/matrix")
+       , messaging: require("dmz/runtime/messaging")
+       , defs: require("dmz/runtime/definitions")
+       , data: require("dmz/runtime/data")
+       , util: require("dmz/types/util")
+       }
+  , editMsg = dmz.messaging.create("Edit_Location_Attributes_Message")
+//  Constants
+  , LocationType = dmz.objectType.lookup("location")
+//  Functions 
+  ;
+
+
+self.shutdown = function () {
+
+  // Reset the text overlays to their original values.
+};
+
+
+dmz.messaging.subscribe("Create_Location_Message", self,  function (data) {
+
+   var pos
+     , obj
+     , out
+     ;
+
+   if (dmz.data.isTypeOf(data)) {
+
+      pos = data.vector("position", 0);
+      obj = data.handle("object", 0);
+
+      if (dmz.util.isUndefined(obj)) { obj = 0; }
+
+      if (dmz.vector.isTypeOf(pos)) {
+
+         out = dmz.data.create();
+
+         if (obj !== 0) {
+
+            if (!dmz.object.type(obj).isOfType(LocationType)) { obj = 0; }
+         }
+
+         if (obj === 0) {
+
+            obj = dmz.object.create("location");
+            dmz.object.position(obj, null, pos);
+            dmz.object.activate(obj);
+
+            out.handle("created", 0, obj);
+         }
+
+         out.handle("object", 0, obj);
+
+         editMsg.send(out);
+      }
+   }
+});
+
+dmz.object.text.observe(self, "Location_Name", function (obj, attr, value) {
+
+   var pop = dmz.object.scalar(obj, "Location_Population")
+     ;
+
+   if (pop) { value = value + ": " + pop.toFixed(); }
+
+   dmz.object.text(obj, "location-text", value);
+});
+
+dmz.object.scalar.observe(self, "Location_Population", function (obj, attr, value) {
+
+   var loc = dmz.object.text(obj, "Location_Name")
+     , str = value.toFixed()
+     ;
+
+   if (loc) { str = loc + ": " + str; }
+
+   dmz.object.text(obj, "location-text", str);
+});
