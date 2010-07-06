b796c678177d17eb2d5884452eae10e29485db68
Randall E. Barker <simstorm@mac.com>
2010-07-06 16:40:35 -0700

    Updated channel switch plugin to be compatible with other channel plugins.

--- scripts/server.js
+++ scripts/server.js
@@ -1,9 +1,37 @@
-var http = require('http');
+var http = require('http')
+  , log = require("sys").log
+  ;
 
 http.createServer(function (req, res) {
 
-  res.writeHead(200, {'content-type': 'text/plain'});
-  res.end('Goodbye World\n');
+  var sizeStr
+    , remain = 0
+    ;
+
+  if (req.method === "GET") {
+
+     res.writeHead(200, {'content-type': 'text/plain'});
+     res.end('Goodbye World\n');
+  }
+  else if (req.method == "PUT") {
+
+     log("Got PUT request");
+     sizeStr = req.headers["content-length"]
+     if (sizeStr) { remain = parseInt(sizeStr); }
+     res.connection.addListener("data", function(chunk) {
+
+         log(chunk);
+         remain -= chunk.length;
+
+         if (remain <= 0) {
+
+            res.writeHead(200, {});
+            res.end();
+         }
+     });
+     res.connection.addListener("end", function() { log("end"); });
+//     res.end();
+  }
 
 }).listen(8124, "127.0.0.1");
 
