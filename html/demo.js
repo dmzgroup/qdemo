2fb3352db6e5fda44348e9546f2fd639cb1798fa
Randall E. Barker <simstorm@mac.com>
2010-07-07 16:24:05 -0700

    Wip

---
+++ html/demo.js
@@ -0,0 +1,104 @@
+(function () {
+
+var processReports
+  , tick
+  , timeout
+  , map = new Image()
+  , Scale = 0.381621621621622
+//  , Scale = 2.616621983914209
+  ;
+
+map.src = "map.png";
+
+processReports = function (reports) {
+
+   var canvas = document.getElementById("view")
+     , keys = Object.keys(reports)
+     , context
+     ;
+
+   if (canvas) {
+
+      context = canvas.getContext("2d");
+
+      if (context) {
+
+         context.clearRect(0, 0, canvas.width, canvas.height);
+
+         context.save();
+         context.moveTo(0, 0);
+         context.drawImage(map, 0, 0);
+         context.restore();
+
+         keys.forEach(function (key) {
+
+            var obj = reports[key]
+              , x
+              , z
+              , lineWidth = context.lineWidth
+              ;
+
+            if (obj.position) {
+
+               context.save();
+
+               context.beginPath();
+               x = Math.floor((obj.position.x + 1902) * Scale);
+               z = Math.floor(600 + ((obj.position.z - 1246) * Scale));
+               context.moveTo(x, z);
+               context.fillStyle = "black";
+               context.arc(x, z, 6, 0, Math.PI * 2);
+               context.fill();
+
+               context.beginPath();
+               if (obj.state === "r") { context.fillStyle = "red"; }
+               else if (obj.state === "y") { context.fillStyle = "yellow"; }
+               else { context.fillStyle = "blue"; }
+
+               context.arc(x, z, 5, 0, Math.PI * 2);
+
+               context.fill();
+
+               context.textBaseline = "middle";
+               context.moveTo (x + 10, z);
+               context.lineWidth = lineWidth + 2;
+               context.strokeStyle = "white";
+               context.strokeText(obj.text, x + 10, z);
+               context.moveTo (x + 10, z);
+               context.lineWidth = lineWidth;
+               context.fillStyle = "black";
+               context.fillText(obj.text, x + 10, z);
+               context.restore();
+            }
+         });
+      }
+   }
+};
+
+
+tick = function () {
+
+   var req = new XMLHttpRequest()
+     , reports
+     ;
+
+   req.open("GET", "http://localhost:5984/demo/data", false);
+//   req.open("GET", "/demo/data", false);
+   req.send();
+
+   if (req.responseText) {
+
+//      window.console.log(req.responseText);
+
+      reports = JSON.parse(req.responseText);
+
+      processReports(reports);
+   }
+
+   timeout = setTimeout(tick, 2000);
+};
+
+
+timeout = setTimeout(tick, 0);
+
+})();
--- scripts/publish.js
+++ scripts/publish.js
@@ -1,32 +1,125 @@
 var dmz =
        { object: require("dmz/components/object")
        , objectType: require("dmz/runtime/objectType")
+       , http: require("dmz/components/http")
        , time: require("dmz/runtime/time")
        , vector: require("dmz/types/vector")
        , matrix: require("dmz/types/matrix")
        , defs: require("dmz/runtime/definitions")
        , util: require("dmz/types/util")
        }
-  , reports = {}
+  , inUpload = false
+  , revInt = 0
+  , reports = { _id: "data" }
+  , publish = false
 //  Constants
+  , DataFile = "http://localhost:5984/demo/data"
   , ReportType = dmz.objectType.lookup("field-report")
+  , YellowState = dmz.defs.lookupState("Yellow")
+  , RedState = dmz.defs.lookupState("Red")
 //  Functions 
+  , getRevInt
+  , timer
   ;
+
+getRevInt = function (rev) {
+
+   var result = 0
+     ;
+
+   if (rev) { result = parseInt(rev.split("-")[0]); }
+
+   return result;
+};
+
+
+timer = function (time) {
+
+   var keys = Object.keys(reports)
+     , out
+     ;
+
+   if (publish && !inUpload) {
+
+      out = JSON.stringify(reports);
+
+      inUpload = true;
+
+      dmz.http.upload(self, DataFile, out, function(value, addr, error) {
+
+         var crev
+           , data = JSON.parse(value)
+           ;
+
+         if (data.ok === true) { self.log.info(value); }
+         else { self.log.error(value); }
+
+         if (data.rev) {
+
+            crev = getRevInt(data.rev);
+
+            if (crev > revInt) {
+
+               reports._rev = data.rev;
+               revInt = crev;
+            }
+         }
+         else { self.log.error(data.error, data.reason); }
+
+         inUpload = false;
+      });
+
+      publish = false;
+   }
+};
+
+
+dmz.http.download(self, DataFile, function(value, error) {
+
+   var data
+     ;
+
+   if (value) {
+
+      data = JSON.parse(value);
+
+      if (data.error && (result.error === "not_found")) {
+
+         inUpload = true;
+         dmz.http.upload(self, DataFile, "{}", function(value, addr, error) {
+
+            var data
+              ;
+
+            if (value) {
+
+               data = JSON.parse(value);
+
+               if (data._rev) {
+
+                  reports._rev = data._rev
+                  revInt = getRevInt(data._rev);
+                  dmz.time.setRepeatingTimer(self, 2, timer);
+               }
+            }
+
+            inUpload = false;
+         });
+      }
+      else if (data._rev) {
+
+         reports._rev = data._rev
+         revInt = getRevInt(data._rev);
+         dmz.time.setRepeatingTimer(self, 2, timer);
+      }
+   }
+});
 
 
 self.shutdown = function () {
 
   // Reset the text overlays to their original values.
 };
-
-
-dmz.time.setRepeatingTimer(self, 2, function (time) {
-
-   var keys = Object.keys(reports)
-     ;
-
-   self.log.error(JSON.stringify(reports));
-});
 
 
 dmz.object.create.observe(self, function (handle, type) {
@@ -42,6 +135,7 @@ dmz.object.create.observe(self, function (handle, type) {
       obj.text = dmz.object.text(handle, "field-report-text");
 
       reports[handle] = obj;
+      publish = true;
    }
 });
 
@@ -51,7 +145,7 @@ dmz.object.destroy.observe(self, function (handle) {
    var obj = reports[handle]
      ;
 
-   if (obj) { delete reports[handle]; }
+   if (obj) { delete reports[handle]; publish = true; }
 });
 
 
@@ -60,7 +154,7 @@ dmz.object.position.observe(self, function (handle, attr, value) {
    var obj = reports[handle]
      ;
 
-   if (obj) { obj.position = value; }
+   if (obj) { obj.position = value; publish = true; }
 });
 
 
@@ -69,7 +163,14 @@ dmz.object.state.observe(self, function (handle, attr, value) {
    var obj = reports[handle]
      ;
 
-   if (obj) { obj.state = value; }
+   if (obj && value) {
+
+      if (value.and(YellowState).bool()) { obj.state = "y"; }
+      else if (value.and(RedState).bool()) { obj.state = "r"; }
+      else { obj.state = "b"; }
+
+      publish = true;
+   }
 });
 
 
@@ -78,6 +179,6 @@ dmz.object.text.observe(self, "field-report-text", function (handle, attr, value
    var obj = reports[handle]
      ;
 
-   if (obj) { obj.text = value; }
+   if (obj) { obj.text = value; publish = true; }
 });
 
